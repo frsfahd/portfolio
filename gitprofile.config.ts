6070317e026806b76b494a203573081d0e44fe59
Farras El-Fahd <farraselfahd2000@gmail.com>
2025-11-25 13:14:12 +0700

    Update project list in gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['afifahreza/cuanpah', 'AZulUye/Inscore-App', 'frsfahd/ai-sec-assist'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['AZulUye/Inscore-App','frsfahd/ai-sec-assist','frsfahd/food_recognizer','frsfahd/go-weather','afiffahreza/cuanpah'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {},
